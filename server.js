e83ad810af61154d07c0fec749d8fa35fd209203
Jill Cunningham <jleighcunningham@gmail.com>
2022-02-27 13:23:29 -0500

    created server.js file with api routes and get requests

---
+++ server.js
@@ -0,0 +1,80 @@
+//Dependencies needed//
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+
+//To manage asynchronous processes//
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//Server setup//
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//To use middleware//
+app.use(express.static('./develop/public'));
+
+//API route for GET request//
+app.get('/api/notes', function(req, res) {
+    readFileAsync('./develop/db/db.json', 'utf8').then(function(data) {
+        notes = [].concat(JSON.parse(data))
+        res.json(notes);
+})
+});
+
+//API route for POST request//
+app.post('/api/notes', function(req, res) {
+    const note = req.body;
+    readFileAsync('./develop/db/db.json', 'utf8').then(function(data){
+        const notes =[].concat(JSON.parse(data));
+        note.id = notes.length + 1
+        notes.push(note);
+        return notes
+    })
+    .then(function(notes) {
+writeFileAsync('./develop/db/db.json', JSON.stringify(notes))
+res.json(notes);
+    })
+});
+
+//API route to DELETE a request//
+app.delete('/api/notes/:id', function(req, res) {
+const deletedId = parseInt(req.params.id);
+readFileAsync('./develop/db/db.json', 'utf8')
+.then(function(data) {
+    const notes = [].concat(JSON.parse(data));
+    const newNotes = []
+    for (let i = 0; i<notes.length; i++) {
+        if(deletedId !==notes[i].id) {
+            newNotes.push(notes[i])
+        }
+    }
+    return newNotes
+})
+.then(function(notes) {
+    writeFileAsync('./develop/db/db.json', JSON.stringify(notes))
+    res.send('Your notes have been saved');
+})
+})
+
+//HTML routing//
+app.get('/notes', function(req,res) {
+    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+});
+
+//app.get('/', function(req,res) {
+//res.sendFile(path.join(__dirname, './develop/public/index.html'));
+//});
+
+app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+});
+
+//get the server to listen//
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
